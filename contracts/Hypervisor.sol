0d556f572aacb8e2375b2549a0560cd0c13a5a34
xyz-cmd <86979474+xyz-cmd@users.noreply.github.com>
2022-10-18 09:56:06 -0400

    semver

--- contracts/Hypervisor.sol
+++ contracts/Hypervisor.sol
@@ -16,7 +16,7 @@ import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 import "@uniswap/v3-core/contracts/libraries/FullMath.sol";
 import "@uniswap/v3-periphery/contracts/libraries/LiquidityAmounts.sol";
 
-/// @title Hypervisor
+/// @title Hypervisor v1.2.0
 /// @notice A Uniswap V2-like interface with fungible liquidity to Uniswap V3
 /// which allows for arbitrary liquidity provision: one-sided, lop-sided, and balanced
 contract Hypervisor is IUniswapV3MintCallback, ERC20Permit, ReentrancyGuard {
